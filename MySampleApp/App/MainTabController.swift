3ee4a93fa1197e8d98e6e583e7e28b66db95a7e9
Ayanna Kosoko <ayannakosoko@gmail.com>
2017-04-04 19:28:31 -0400

    changed

--- MySampleApp/App/MainTabController.swift
+++ MySampleApp/App/MainTabController.swift
@@ -17,6 +17,7 @@ class MainTabController: UITabBarController {
     var signOutObserver: AnyObject!
     var willEnterForegroundObserver: AnyObject!
     fileprivate let loginButton: UIBarButtonItem = UIBarButtonItem(title: nil, style: .done, target: nil, action: nil)
+    let button = UIButton(type: UIButtonType.custom)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,7 @@ class MainTabController: UITabBarController {
         }
         
         presentSignInViewController()
+        setupMiddleButton()
 
         let myrepssb = UIStoryboard(name: "NoSQLDatabase", bundle: nil)
         let myrepsvc = myrepssb.instantiateViewController(withIdentifier: "NoSQLDatabase")
@@ -48,11 +50,10 @@ class MainTabController: UITabBarController {
         repsnav.tabBarItem.title = "My Reps"
         repsnav.tabBarItem.image = UIImage(named: "EngageSmall")
         
-        menav.tabBarItem.title = "Me"
+        menav.tabBarItem.title = "Profile"
         menav.tabBarItem.image = UIImage(named: "UserProfileDataSmall")
-        
-        
-        
+       
+
         signInObserver = NotificationCenter.default.addObserver(forName: NSNotification.Name.AWSIdentityManagerDidSignIn, object: AWSIdentityManager.default(), queue: OperationQueue.main, using: {[weak self] (note: Notification) -> Void in
             guard let strongSelf = self else { return }
             print("Sign In Observer observed sign in.")
@@ -78,6 +79,8 @@ class MainTabController: UITabBarController {
         NotificationCenter.default.removeObserver(willEnterForegroundObserver)
     }
     
+    
+    
     func setupRightBarButtonItem() {
         navigationItem.rightBarButtonItem = loginButton
         navigationItem.rightBarButtonItem!.target = self
@@ -94,6 +97,35 @@ class MainTabController: UITabBarController {
             let viewController = storyboard.instantiateViewController(withIdentifier: "SignIn")
             self.present(viewController, animated: true, completion: nil)
         }
+    }
+    
+    func setupMiddleButton() {
+        let menuButton = UIButton(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
+        
+        var menuButtonFrame = menuButton.frame
+        menuButtonFrame.origin.y = view.bounds.height - menuButtonFrame.height
+        menuButtonFrame.origin.x = view.bounds.width/2 - menuButtonFrame.size.width/2
+        menuButton.frame = menuButtonFrame
+        
+        menuButton.backgroundColor = UIColor.red
+        menuButton.layer.cornerRadius = menuButtonFrame.height/2
+        view.addSubview(menuButton)
+
+        
+        menuButton.setImage(UIImage(named: "logo"), for: .normal)
+        menuButton.addTarget(self, action: #selector(menuButtonAction), for: .touchUpInside)
+        view.addSubview(menuButton)
+        
+        
+        view.layoutIfNeeded()
+    }
+    
+    @objc private func menuButtonAction(sender: UIButton){
+            let storyboard = UIStoryboard(name: "SignIn", bundle: nil)
+            let viewController = storyboard.instantiateViewController(withIdentifier: "SignIn")
+            self.present(viewController, animated: true, completion: nil)
+        
+    
     }
     
     func handleLogout() {
